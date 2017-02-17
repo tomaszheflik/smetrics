907cfa7732e18669ee5eaa2965cfae140a44c15e
Tomasz Heflik <heflik@gmail.com>
2017-02-17 23:42:32 +0100

    Add Json configuration

---
+++ smetrics.go
@@ -0,0 +1,32 @@
+import (
+    "encoding/json"
+    "os"
+    "fmt"
+)
+
+type Host struct {
+  url       []string
+  port      int
+}
+
+type Mesos struct {
+    hosts   []Host
+}
+type K8s struct {
+    hosts   []Host
+}
+
+type Configuration struct {
+  Mesos   Mesos
+  K8s     K8s
+}
+
+
+file, _ := os.Open("conf.json")
+decoder := json.NewDecoder(file)
+configuration := Configuration{}
+err := decoder.Decode(&configuration)
+if err != nil {
+  fmt.Println("error:", err)
+}
+fmt.Println(configuration.Users) // output: [UserA, UserB]
